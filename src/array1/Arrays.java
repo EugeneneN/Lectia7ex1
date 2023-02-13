ccb80b36a55e1d8daab3b7e92653905fc11f4557
EugeneneN <30august89@gmail.com>
2023-02-13 23:43:28 +0100

    Arrays

---
+++ src/array1/Arrays.java
@@ -0,0 +1,21 @@
+package array1;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] size;
+        size = new int[5] ;
+        size[0] = 2;
+        size[1] = 4;
+        size[2] = 6;
+        size[3] = 8;
+        size[4] = 10;
+        System.out.println("The size is:" + size[0] );
+        System.out.println("The size is:" + size[1] );
+        System.out.println("The size is:" + size[2] );
+        System.out.println("The size is:" + size[3] );
+        System.out.println("The size is:" + size[4] );
+    }
+
+
+    }
+
